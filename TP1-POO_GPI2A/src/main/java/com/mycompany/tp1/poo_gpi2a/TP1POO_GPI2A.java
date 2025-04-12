cff422673669fd254dd647a5a941fb3d0d0e30a8
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-12 18:01:36 -0300

    carga de notas ejercicio 1 - tp1

--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
@@ -1,8 +1,27 @@
 package com.mycompany.tp1.poo_gpi2a;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class TP1POO_GPI2A {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // EJERCIRIO 1
+        Scanner scanner = new Scanner(System.in);
+        int A_CARGAR;
+        float[] NOTAS;
+        
+        System.out.println("Ingrese la cantidad de notas a cargar: ");
+        A_CARGAR = scanner.nextInt();
+        
+        NOTAS = new float[A_CARGAR];
+        
+        for (int i = 0; i < A_CARGAR; i++) {
+            System.out.println("Ingrese la siguiente nota a cargar:");
+            NOTAS[i] = scanner.nextFloat();
+        }
+        
+        System.out.println(Arrays.toString(NOTAS));
+        
+        scanner.close();
     }
 }
