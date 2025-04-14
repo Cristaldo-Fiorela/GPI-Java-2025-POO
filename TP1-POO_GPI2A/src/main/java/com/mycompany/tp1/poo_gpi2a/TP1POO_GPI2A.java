1593fa976d9633414e07d2de76c5e087a75a84d5
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-14 15:42:42 -0300

    comienzo de ejercicio 3 TP1

--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
@@ -10,6 +10,7 @@ public class TP1POO_GPI2A {
         int A_CARGAR;
         float[] NOTAS;
         float MAYOR = 0;
+        // EJERCICIO 2
         int APROBADOS = 0;
         int DESAPROBADOS = 0;
         
@@ -48,6 +49,27 @@ public class TP1POO_GPI2A {
         
         System.out.println("La mayor nota cargada es: " + MAYOR); 
         System.out.println("Hay " + APROBADOS + " aprobados y " + DESAPROBADOS + " desaprobados");
+        
+        // EJERCICIO 3
+        int[] CANTIDADES;
+        double[] COSTOS;
+        int PRODUCTOS;
+        
+        System.out.println("Ingrese el total de productos a cargar: ");
+        PRODUCTOS = scanner.nextInt();
+        
+        CANTIDADES = new int[PRODUCTOS];
+        COSTOS = new double[PRODUCTOS];
+        
+        for (int i = 0; i < PRODUCTOS; i++) {
+            System.out.println("CANTIDAD de producto #" + (i+1) + ":");
+            CANTIDADES[i] = scanner.nextInt();
+            System.out.println("VALOR de producto #" + (i+1) + ":");
+            COSTOS[i] = scanner.nextDouble();
+        }
+        
+        System.out.println(Arrays.toString(CANTIDADES));
+        System.out.println(Arrays.toString(COSTOS));
         
         scanner.close();
     }
