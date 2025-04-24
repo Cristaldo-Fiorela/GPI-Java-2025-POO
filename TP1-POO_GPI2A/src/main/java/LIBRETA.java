713062d551a691e594bdba6582e236c047f5fc53
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2025-04-24 18:51:15 -0300

    avance de separacion a clases de main

--- TP1-POO_GPI2A/src/main/java/LIBRETA.java
+++ TP1-POO_GPI2A/src/main/java/LIBRETA.java
@@ -1,12 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author Fio
- */
 public class LIBRETA {
+     // EJERCICIO 1
+    Scanner scanner = new Scanner(System.in);
+    int A_CARGAR;
+    float[] NOTAS;
+    float MAYOR = 0;
+    // EJERCICIO 2
+    int APROBADOS = 0;
+    int DESAPROBADOS = 0;
+    
+    public void setNotas() {
+        System.out.println("Ingrese la cantidad de notas a cargar: ");
+        A_CARGAR = scanner.nextInt();
+        
+        NOTAS = new float[A_CARGAR];
+        
+        for (int i = 0; i < A_CARGAR; i++) {
+            float nota = -1;
+            
+            while (nota < 0 || nota > 10) {
+                System.out.println("Ingrese la siguiente nota a cargar:");
+                nota = scanner.nextFloat();
+
+                if (nota < 0 || nota > 10) {
+                    System.out.println("Nota invalida. Debe estar entre 0 y 10.");
+                }
+            }
+            
+            NOTAS[i] = nota;
+        }
+    }
+    
+    public void getMayorNota() {
+        
+        if (NOTAS.length > 0) {         
+            for (float nota : NOTAS) {
+                if (nota > MAYOR) {
+                    MAYOR = nota;
+                }
+            }  
+        } else  {
+            System.out.println("U");
+        }
+
+    }
     
 }
--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
@@ -5,34 +5,8 @@ import java.util.Scanner;
 public class TP1POO_GPI2A {
 
     public static void main(String[] args) {
-        // EJERCICIO 1
-        Scanner scanner = new Scanner(System.in);
-        int A_CARGAR;
-        float[] NOTAS;
-        float MAYOR = 0;
-        // EJERCICIO 2
-        int APROBADOS = 0;
-        int DESAPROBADOS = 0;
         
-        System.out.println("Ingrese la cantidad de notas a cargar: ");
-        A_CARGAR = scanner.nextInt();
         
-        NOTAS = new float[A_CARGAR];
-        
-        for (int i = 0; i < A_CARGAR; i++) {
-            float nota = -1;
-            
-            while (nota < 0 || nota > 10) {
-                System.out.println("Ingrese la siguiente nota a cargar:");
-                nota = scanner.nextFloat();
-
-                if (nota < 0 || nota > 10) {
-                    System.out.println("Nota invalida. Debe estar entre 0 y 10.");
-                }
-            }
-            
-            NOTAS[i] = nota;
-        }
         
         for (float nota : NOTAS) {
             if (nota > MAYOR) {
