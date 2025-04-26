73f5e0b708706f0a175a6997aa3b427b5722eea5
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-26 14:49:04 -0300

    refactorizacion de codigo ejercicios 3

---
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/PRODUCTOS.java
@@ -0,0 +1,49 @@
+package com.mycompany.tp1.poo_gpi2a;
+
+import java.util.Scanner;
+
+
+public class PRODUCTOS {
+    private Scanner scanner = new Scanner(System.in);
+    private int[] CANTIDADES;
+    private double[] COSTOS;
+    
+    public PRODUCTOS() {
+    }
+
+    public PRODUCTOS(int[] CANTIDADES, double[] COSTOS) {
+        this.CANTIDADES = CANTIDADES;
+        this.COSTOS = COSTOS;
+    }
+    
+    public void setProductos() {
+        int A_CARGAR;
+        
+        System.out.println("Ingrese el total de productos a cargar: ");
+        A_CARGAR = scanner.nextInt();
+        
+        CANTIDADES = new int[A_CARGAR];
+        COSTOS = new double[A_CARGAR];
+
+         for (int i = 0; i < A_CARGAR; i++) {
+            System.out.println("CANTIDAD de producto #" + (i+1) + ":");
+            CANTIDADES[i] = scanner.nextInt();
+            System.out.println("VALOR de producto #" + (i+1) + ":");
+            COSTOS[i] = scanner.nextDouble();
+        }
+    }
+    
+    public float getTotal() {
+        if (CANTIDADES.length == 0 || COSTOS.length == 0) {
+            throw new IllegalStateException("No hay datos existentes, ejecute el metodo setProductos() para realizar su carga");
+        }
+        
+        float TOTAL = 0;
+        
+        
+        return TOTAL;
+    }
+    
+
+
+}
--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
@@ -1,12 +1,8 @@
 package com.mycompany.tp1.poo_gpi2a;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class TP1POO_GPI2A {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+    public static void main(String[] args) {        
         // EJERCICIO 1
         LIBRETA libreta = new LIBRETA();
         
@@ -15,32 +11,7 @@ public class TP1POO_GPI2A {
         
         // EJERCICIO 2
         libreta.getAlumnosAprobadosDesaprobados();
-        
-        /*
-        // EJERCICIO 3
-        int[] CANTIDADES;
-        double[] COSTOS;
-        int PRODUCTOS;
-        
-        System.out.println("Ingrese el total de productos a cargar: ");
-        PRODUCTOS = scanner.nextInt();
-        
-        CANTIDADES = new int[PRODUCTOS];
-        COSTOS = new double[PRODUCTOS];
-        
-        for (int i = 0; i < PRODUCTOS; i++) {
-            System.out.println("CANTIDAD de producto #" + (i+1) + ":");
-            CANTIDADES[i] = scanner.nextInt();
-            System.out.println("VALOR de producto #" + (i+1) + ":");
-            COSTOS[i] = scanner.nextDouble();
-        }
-        
-        System.out.println(Arrays.toString(CANTIDADES));
-        System.out.println(Arrays.toString(COSTOS));
-        
-        scanner.close();
 
-        */
     }
 
 }
