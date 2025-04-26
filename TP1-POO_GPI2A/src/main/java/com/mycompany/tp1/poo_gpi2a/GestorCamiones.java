bec725f5c4317869dc7c198eef80946c84b99350
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-26 16:19:19 -0300

    seteando datos para ejercicio 4

--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/GestorCamiones.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/GestorCamiones.java
@@ -1,20 +1,63 @@
 package com.mycompany.tp1.poo_gpi2a;
 
+import java.util.Scanner;
+
 public class GestorCamiones {
     private int totalCamiones;
+    private int cantidadDatosAPedir;
     private String[][] planilla;
 
     public GestorCamiones() {
-        this.totalCamiones = 30;
-        this.planilla = new String[totalCamiones][5];
+        this.totalCamiones = 30;        
+        this.cantidadDatosAPedir = 4;
+        this.planilla = new String[totalCamiones][cantidadDatosAPedir];
     }
 
-    public GestorCamiones(int totalCamiones) {
+    public GestorCamiones(int totalCamiones, int cantidadDatosAPedir) {
         this.totalCamiones = totalCamiones;
-        this.planilla = new String[totalCamiones][5];
+        this.cantidadDatosAPedir = cantidadDatosAPedir;
+        this.planilla = new String[totalCamiones][cantidadDatosAPedir];
     }
     
     public void setDatos() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("A continuacion ingrese los datos pedidos de los camiones:");
+        
+        for (int fila = 0; fila < totalCamiones; fila++) {
+            for (int columna = 0; columna < cantidadDatosAPedir; columna++) {
+                switch(columna) {
+                    case 0:
+                        System.out.println("Ingrese la PATENTE del camion #" + fila + 1);
+                        planilla[fila][columna] = scanner.next().toLowerCase();
+                        break;
+                    case 1:
+                        System.out.println("Ingrese el NOMBRE y APELLIDO del chofer del camion #" + fila + 1);
+                        planilla[fila][columna] = scanner.next().toLowerCase();
+                        break;
+                    case 2:
+                        boolean cargaValida = false;
+                        
+                        while(!cargaValida) {
+                            System.out.println("Ingrese el TIPO DE CARGA (te, yerba o mate) del camion #" + fila + 1);
+                            String carga = scanner.next().toLowerCase();
+                            if (carga == "te" || carga == "té" || carga == "yerba" || carga == "mate") {
+                                planilla[fila][columna] = carga;
+                                cargaValida = true;
+                            } else {
+                                System.out.println("Carga invalida. Ingrese: te, yerba o mate");
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Ingrese la HORA DE EGRESO del camion #" + fila + 1);
+                        planilla[fila][columna] = scanner.next().toLowerCase();
+                        break;
+                }
+                
+            }
+            
+        }
         
     }
+
 }
