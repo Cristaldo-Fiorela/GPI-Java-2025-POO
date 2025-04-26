ba51008fc1bfbed2ab30b9bc1b2ab82d10b9f49b
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-26 15:29:08 -0300

    inicio ejercicio 4

---
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/GestorCamiones.java
@@ -0,0 +1,15 @@
+package com.mycompany.tp1.poo_gpi2a;
+
+public class GestorCamiones {
+    private int totalCamiones;
+    private String[][] planilla = new String[totalCamiones][5];
+
+    public GestorCamiones() {
+    }
+
+    public GestorCamiones(int totalCamiones) {
+        this.totalCamiones = totalCamiones;
+    }
+    
+    
+}
--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/TP1POO_GPI2A.java
@@ -2,7 +2,9 @@ package com.mycompany.tp1.poo_gpi2a;
 
 public class TP1POO_GPI2A {
 
-    public static void main(String[] args) {        
+    public static void main(String[] args) { 
+        /*
+        
         // EJERCICIO 1
         LIBRETA libreta = new LIBRETA();
         
@@ -15,10 +17,13 @@ public class TP1POO_GPI2A {
         // EJERCICIO 3
         PRODUCTOS productos = new PRODUCTOS();
         
-        
         productos.setProductos();
         System.out.println("El total de los productos ingresados es: $" + productos.getTotal());
         System.out.println("El total de los productos MAYORES a $1000 es: " + productos.getMayorAMil());
+        
+       */
+        
+        // EJERCICIO 4
 
     }
 
