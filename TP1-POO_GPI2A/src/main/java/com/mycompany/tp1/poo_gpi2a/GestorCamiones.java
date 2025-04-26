3d98e87d5b9b8bdb07679583369c443288868623
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-26 15:34:30 -0300

    correccion de constructor y valores por default

--- TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/GestorCamiones.java
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/GestorCamiones.java
@@ -2,14 +2,19 @@ package com.mycompany.tp1.poo_gpi2a;
 
 public class GestorCamiones {
     private int totalCamiones;
-    private String[][] planilla = new String[totalCamiones][5];
+    private String[][] planilla;
 
     public GestorCamiones() {
+        this.totalCamiones = 30;
+        this.planilla = new String[totalCamiones][5];
     }
 
     public GestorCamiones(int totalCamiones) {
         this.totalCamiones = totalCamiones;
+        this.planilla = new String[totalCamiones][5];
     }
     
-    
+    public void setDatos() {
+        
+    }
 }
