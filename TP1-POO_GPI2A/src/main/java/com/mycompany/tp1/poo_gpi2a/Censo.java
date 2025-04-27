7065311c4e680723052b7eb09c0d5e26d6fcd400
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-04-27 17:57:05 -0300

    ejercicio 7 - declaracion y constructores

---
+++ TP1-POO_GPI2A/src/main/java/com/mycompany/tp1/poo_gpi2a/Censo.java
@@ -0,0 +1,26 @@
+package com.mycompany.tp1.poo_gpi2a;
+
+public class Censo {
+    private String[][] censo;
+
+    public Censo() {
+    }
+
+    public Censo(String[][] censo) {
+        this.censo = censo;
+    }
+    
+        
+    
+    /*
+        DNI, EDAD, SEXO
+        si se ingresa 0 debe parar de cargar datos en DNI
+        informar:
+            - total censado arr leng
+            - cantidad varones
+            - cantidad mujeres
+            - porcentaje VARONES entre 16 y 65 respecto al TOTAL de varones
+            - mostrar todos los datos de la persona con mayor edad
+    */
+
+}
