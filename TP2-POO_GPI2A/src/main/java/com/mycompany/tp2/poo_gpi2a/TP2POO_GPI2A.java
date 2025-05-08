1f64e42c5ddf15626f9e1f328c5bb932a056b0b7
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2025-05-08 18:28:03 -0300

    instanciando clases en main

--- TP2-POO_GPI2A/src/main/java/com/mycompany/tp2/poo_gpi2a/TP2POO_GPI2A.java
+++ TP2-POO_GPI2A/src/main/java/com/mycompany/tp2/poo_gpi2a/TP2POO_GPI2A.java
@@ -3,5 +3,13 @@ package com.mycompany.tp2.poo_gpi2a;
 public class TP2POO_GPI2A {
 
     public static void main(String[] args) {
+        // Instanciando clases creadas
+        Adultos adulto = new Adultos();
+        Pediatricos pediatrico = new Pediatricos();
+        Internados internado = new Internados();
+        
+        // Adultos
+        System.out.println(adulto.rx());
+        
     }
 }
