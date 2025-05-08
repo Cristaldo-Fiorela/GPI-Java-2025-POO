9c4ffcf303a56a590d3fa41ecf37caf534d27372
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2025-05-08 19:48:27 -0300

    pruebas de meodos de clases en main

--- TP2-POO_GPI2A/src/main/java/com/mycompany/tp2/poo_gpi2a/TP2POO_GPI2A.java
+++ TP2-POO_GPI2A/src/main/java/com/mycompany/tp2/poo_gpi2a/TP2POO_GPI2A.java
@@ -3,13 +3,28 @@ package com.mycompany.tp2.poo_gpi2a;
 public class TP2POO_GPI2A {
 
     public static void main(String[] args) {
+        // Variables para utilizar
+        double precio = 50000;
+        
         // Instanciando clases creadas
         Adultos adulto = new Adultos();
         Pediatricos pediatrico = new Pediatricos();
         Internados internado = new Internados();
         
         // Adultos
-        System.out.println(adulto.rx());
+        System.out.println("El precio es $" + precio);
+        System.out.println("El descuento de adultos es: "+ (adulto.descuento() * 100) + "%");
+        System.out.println("Con descuento de adultos queda: $"+ (precio - (precio * (adulto.descuento() / 100))));
         
+        // Pediatricos
+        System.out.println("El precio es $" + precio);
+        System.out.println("El descuento de adultos es: "+ (pediatrico.descuento() * 100) + "%");
+        System.out.println("Con descuento de adultos queda: $"+ (precio - (precio * (pediatrico.descuento() / 100))));
+        
+        // Internados
+        System.out.println("El precio es $" + precio);
+        System.out.println("El descuento de adultos es: "+ (internado.descuento() * 100) + "%");
+        System.out.println("Con descuento de adultos queda: $"+ (precio - (precio * (internado.descuento() / 100))));
+
     }
 }
