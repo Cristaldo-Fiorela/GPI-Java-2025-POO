71649bffc2171598567a3118bb995745525af677
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-05-30 16:16:33 -0300

    inicio TP 3

---
+++ TP3-POO_GPI2A/src/main/java/logica/TP3POO_GPI2A.java
@@ -0,0 +1,11 @@
+package logica;
+
+import igu.CalculadoraIMC;
+
+public class TP3POO_GPI2A {
+
+    public static void main(String[] args) {
+        CalculadoraIMC calculadora = new CalculadoraIMC();
+        
+    }
+}
