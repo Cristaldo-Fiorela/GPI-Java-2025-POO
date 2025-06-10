54b6250a5ffad31263d3f16bc1e45a2fa8c223dc
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-10 15:44:01 -0300

    creacion clase Persona. Nombre de variables renombradas

--- TP4-POO_GPI2A/src/main/java/logica/TP4POO_GPI2A.java
+++ TP4-POO_GPI2A/src/main/java/logica/TP4POO_GPI2A.java
@@ -1,6 +1,8 @@
 package logica;
 
 import igu.AgendaUI;
+import java.awt.List;
+import java.util.ArrayList;
 
 public class TP4POO_GPI2A {
 
@@ -8,5 +10,8 @@ public class TP4POO_GPI2A {
         AgendaUI agendaUI = new AgendaUI();
         agendaUI.setVisible(true);
         agendaUI.setLocationRelativeTo(null);
+        
+        // List<Persona> dataAgenda = new ArrayList<Persona>();
+        
     }
 }
