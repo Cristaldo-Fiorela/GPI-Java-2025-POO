ccb934112e5e1d88296505ef8accde5260f7b6ad
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-20 18:08:01 -0300

    vista con JFrame de GUI

---
+++ TP1-AS_GPI2A/src/main/java/modelo/TP1AS_GPI2A.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import vista.GUI;
+
+public class TP1AS_GPI2A {
+
+    public static void main(String[] args) {
+        GUI biblioteca = new GUI();
+        biblioteca.setVisible(true);
+        biblioteca.setLocationRelativeTo(null);
+    }
+}
