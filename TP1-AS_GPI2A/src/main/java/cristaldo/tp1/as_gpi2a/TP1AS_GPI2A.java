bab1319d1db9759bf25d28d96de832529636f843
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-20 17:12:04 -0300

    comienzo de TP ejercitacion de Arquitectura de Software

---
+++ TP1-AS_GPI2A/src/main/java/cristaldo/tp1/as_gpi2a/TP1AS_GPI2A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cristaldo.tp1.as_gpi2a;
+
+/**
+ *
+ * @author Fio
+ */
+public class TP1AS_GPI2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
