f6aaee6a54ed37274433c61e9bc975ba74fd6bd3
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-20 18:59:08 -0300

    correccion de nombre DB

--- TP1-AS_GPI2A/src/main/java/modelo/clases/DAOImplPostgreSQL.java
+++ TP1-AS_GPI2A/src/main/java/modelo/clases/DAOImplPostgreSQL.java
@@ -4,32 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 import modelo.interfaces.IBibliotecaDAO;
 
-public class DAOImplProstgreSQL implements IBibliotecaDAO{
-    private static List<Libro> bibliotecaProst = new ArrayList<>();
+public class DAOImplPostgreSQL implements IBibliotecaDAO{
+    private static List<Libro> bibliotecaPost = new ArrayList<>();
     private static int contador = 1;
 
     @Override
     public List<Libro> obtenerLibros() {
-        return new ArrayList<>(bibliotecaProst);
+        return new ArrayList<>(bibliotecaPost);
     }
 
     @Override
     public boolean agregarLibro(Libro libro) {
         libro.setId(contador++);
-        return bibliotecaProst.add(libro);
+        return bibliotecaPost.add(libro);
     }
 
     @Override
     public boolean eliminarLibro(int id) {
-        for (int i = 0; i < bibliotecaProst.size(); i++) {
+        for (int i = 0; i < bibliotecaPost.size(); i++) {
         
-            if( bibliotecaProst.get(i).getId() == id) {
-                bibliotecaProst.remove(i);
+            if( bibliotecaPost.get(i).getId() == id) {
+                bibliotecaPost.remove(i);
                 return true;
             }
         }
         return false;
     }
-    
-    
 }
