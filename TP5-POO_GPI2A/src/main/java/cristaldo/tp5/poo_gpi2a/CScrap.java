ca11c71d3502481db806c7ce05f66f60f6ca091a
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-21 16:05:12 -0300

    scrapping

---
+++ TP5-POO_GPI2A/src/main/java/cristaldo/tp5/poo_gpi2a/CScrap.java
@@ -0,0 +1,49 @@
+package cristaldo.tp5.poo_gpi2a;
+
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class CScrap {
+    
+    public void ScrapSitioWeb(JTextField urlSitio, JTable resultado ) {
+        String url = urlSitio.getText();
+        
+        try {
+            Document doc = Jsoup.connect(url).get(); // obtiene info de la URL
+            System.out.println("Conexion Exitosa");
+            
+            // clases de etiquetas a buscar info
+            Elements productos = doc.select("a.poly-component__title");
+            Elements precio = doc.select("div.poly-price__current");
+            Elements preciosOferta = doc.select("div.poly-component__price");
+                        
+            // seteando tabla y nombre de valores de columnas
+            DefaultTableModel modelo = new DefaultTableModel(new Object[] {"Titulo", "Precio", "Precio Oferta", "Enlace"}, 0);
+            resultado.setModel(modelo);
+            
+            // limpieza de tabla
+            modelo.setRowCount(0);
+            
+            for (int i = 0; i < productos.size(); i++) {
+                // info a obtener de etiquetas
+                String titulo = productos.get(i).text();
+                String precioNormal = precio.get(i).select("span.andes-money-amount__fraction").first().text();
+                String preciosOfertaFinal = preciosOferta.get(i).select("span.andes-money-amount__fraction").text();
+                String enlace = productos.get(i).attr("href");
+                
+                // llenando columnas con info obtenida
+                modelo.addRow(new Object[] {titulo, precioNormal, preciosOfertaFinal,enlace});
+            }
+            
+        } catch (Exception e) {
+            DefaultTableModel model = new DefaultTableModel(new Object[] {"Error", "Mensaje"}, 0);
+            resultado.setModel(model);
+            model.setRowCount(0);
+            model.addRow(new Object[] {"Error", e.getMessage()});
+        }
+    }
+}
--- TP5-POO_GPI2A/src/main/java/cristaldo/tp5/poo_gpi2a/TP5POO_GPI2A.java
+++ TP5-POO_GPI2A/src/main/java/cristaldo/tp5/poo_gpi2a/TP5POO_GPI2A.java
@@ -3,6 +3,9 @@ package cristaldo.tp5.poo_gpi2a;
 public class TP5POO_GPI2A {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FormScraping objetoForm = new FormScraping();
+        
+        objetoForm.setVisible(true);
+        objetoForm.setLocationRelativeTo(null);
     }
 }
